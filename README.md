dab3a12c2d9b2527a0001b4dbb69465ec7ffa804
Alkivi <admin@alkivi.fr>
2013-10-04 13:36:14 +0200

    added openerp helper

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ alkivi-api
 alkivi-api is our python module used for scripts.
 So far it can interact with :
 - OVH API (api.ovh.com)
-- Request Tracker (ticketing system)
+- OpenERP api
 - Databases using SqlAlchemy ORM
 
 **Module architecture is still in beta, we are currently learning how to code in Python the right way ;)**
@@ -15,6 +15,8 @@ Dependencies
 -------
 API OVH : python-requests
 ORM     : python-sqlalchemy
+LDAP    : python-ldap
+OpenERP : pip install oerplib
 
 
 Installation
@@ -35,53 +37,7 @@ echo "applicationKey:applicationSecret:consumerKey" > path/.secureData/api_ovh/p
 Examples
 -------
 
-Now let's create a simple python file test.py (check samples/OVH.py)
-```python
-#!/usr/bin/python
-
-import sys
-reload(sys)
-sys.setdefaultencoding("utf-8")
-sys.path.append('/')
-
-#
-# Define Logger : check samples script in alkivi-api/sample/testLogger.py
-#
-from alkivi.common import logger
-from alkivi.common.logger import Logger
-logger = Logger.instance(
-        min_log_level_to_mail  = None,
-        min_log_level_to_save  = logger.DEBUG,
-        min_log_level_to_print = logger.DEBUG,
-        emails=['anthony@alkivi.fr'])
-
-
-#
-# Define OVH API
-#
-from alkivi.api import ovh
-api = ovh.API(useData='alkivi')
-
-
-#
-# Basic Me Info
-#
-from alkivi.api.ovh import me
-OVH_Me = me.OVH_Me(api=api)
-logger.log(OVH_Me.getMyInfo())
-
-
-#
-# Init a pca
-#
-serviceName    = 'toto'
-pcaServiceName = 'toto'
-from alkivi.api.ovh import PCA
-OVH_PCA = PCA.OVH_PCA(api=api, serviceName=serviceName, pcaServiceName=pcaServiceName)
-logger.log(OVH_PCA.getSessions())
-```
-
-
+check samples directory :)
 
 
 License
---
+++ api/openerp/__init__.py
@@ -0,0 +1,80 @@
+# Based on ovh_app of Antoine Sirinelli <antoine@monte-stello.com> 
+# https://github.com/asirinelli/ovh_app
+
+import oerplib
+import json
+import time
+import hashlib
+import os
+import re
+
+import sys
+reload(sys)
+sys.setdefaultencoding("utf-8")
+sys.path.append('/')
+
+from alkivi.common import logger
+
+class API:
+
+    def __init__(self, useData,
+                protocol = 'xmlrpc',
+                port     = '8069',
+                url      = 'localhost',
+                version  = '7.0' ):
+
+        self.protocol = 'xmlrpc'
+        self.port     = port
+        self.url      = url
+        self.dbname   = 'openerp'
+
+        # Going to extract data from /alkivi/.secureData
+        base = '/alkivi/.secureData/api_openerp/'
+        file = base + useData
+
+        if(not(os.path.exists(file))):
+            logger.warning('Unable to fetch correct file. Check that %s exists and is readable' % (file))
+            raise
+        else:
+            # Open file
+            f = open(file)
+
+            # Check syntax
+            rx = re.compile('^(.*?):(.*?):(.*?)$')
+            for line in f:
+                m = rx.search(line)
+                if(m):
+                    self.user, self.password, self.dbname= m.groups()
+
+
+        # Check that we are good to go, dont check consumerKey, we might just want to get credentials ...
+        if(not(self.user) or not(self.password)):
+            logger.warning('Did not find any file that contains correct data, bazinga')
+            raise
+        else:
+            pass
+
+        # First login and keep uid
+        self.oerp = oerplib.OERP(url , protocol=protocol, port=port, version=version)
+        self.user = self.oerp.login(self.user, self.password, self.dbname)
+
+    def execute(self, *args, **kwargs):
+        return self.oerp.execute(*args, **kwargs)
+
+    def get(self, *args, **kwargs):
+        return self.oerp.get(*args, **kwargs)
+
+    def read(self, *args, **kwargs):
+        return self.oerp.read(*args, **kwargs)
+
+    def search(self, *args, **kwargs):
+        return self.oerp.search(*args, **kwargs)
+
+    def create(self, *args, **kwargs):
+        return self.oerp.create(*args, **kwargs)
+
+    def write(self, *args, **kwargs):
+        return self.oerp.write(*args, **kwargs)
+
+    def browse(self, *args, **kwargs):
+        return self.oerp.browse(*args, **kwargs)
