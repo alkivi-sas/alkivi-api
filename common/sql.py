58487427450fe3a70da09a9acdd3ba2c83260e18
Alkivi <admin@alkivi.fr>
2014-01-14 16:17:46 +0100

    pylint

--- common/sql.py
+++ common/sql.py
@@ -1,9 +1,9 @@
-from sqlalchemy import create_engine, ForeignKey
-from sqlalchemy import Column, Date, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship, backref
+"""
+Global wrapper to use to fetch object in our database
+"""
+
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-import datetime
 import os
 import re
 
@@ -13,102 +13,116 @@ from alkivi.common import logger
  
 @Singleton
 class Db(object):
- 
-    '''
+    """
     The DB Class should only exits once, thats why it has the @Singleton decorator.
     To Create an instance you have to use the instance method:
         db = Db.instance()
-    '''
+    """
  
-    engine = None
-    session = None
+    #engine = None
+    #session = None
  
-    def __init__(self, useData, echo=False, charset='utf8'):
+    def __init__(self, use_data, echo=False, charset='utf8'):
 
         # Going to extract data from /alkivi/.secureData
         base = '/alkivi/.secureData/sql/'
-        file = base + useData
+        credentials = base + use_data
 
         # Test file is ok
-        if(not(os.path.exists(file))):
-            logger.warning('Unable to fetch correct file. Check that %s exists and is readable' % (file))
-            raise
+        if not os.path.exists(credentials):
+            raise Exception(
+                'Unable to fetch credentials.' +
+                'Check that %s exists and is readable' % (credentials))
 
         # Open file
-        f = open(file)
+        f_handler = open(file)
 
         # Check syntax
-        rx = re.compile('^(.*?):(.*?):(.*?):(.*?):(.*?):(.*?)$')
-        type,host,port,db,user,password = (None, None, None, None, None, None)
-        for line in f:
-            m = rx.search(line)
-            if(m):
-                type,host,port,db,user,password = m.groups()
+        regexp = re.compile('^(.*?):(.*?):(.*?):(.*?):(.*?):(.*?)$')
+        db_type = None
+        host = None
+        port = None
+        db_name = None
+        user = None
+        password = None
 
-        if(not(password)):
-            logger.warning('Password is not set, meaning that %s file is not correctly formatted, check it out' % file)
-            raise
+        for line in f_handler:
+            match = regexp.search(line)
+            if match:
+                db_type, host, port, db_name, user, password = match.groups()
 
-        if(type == 'mysql'):
-            self.url = '%s://%s:%s@%s:%s/%s?charset=%s' % (type, user, password, host, port, db, charset)
-        elif(type == 'postgres'):
-            self.url = '%s://%s:%s@%s:%s/%s' % (type, user, password, host, port, db )
+        if not password:
+            raise Exception('Password is not set.' +
+                            ' Check that file %s is correct' % credentials)
+
+        if db_type == 'mysql':
+            self.url = '%s://%s:%s@%s:%s/%s?charset=%s' % (db_type, user, 
+                                                           password, host, 
+                                                           port, db_name, 
+                                                           charset)
+        elif db_type == 'postgres':
+            self.url = '%s://%s:%s@%s:%s/%s' % (db_type, user, password, 
+                                                host, port, db_name )
         else:
-            logger.warning('Unknow engin type %s' % type)
-            raise
+            raise Exception('Unknow engin type %s' % db_type)
 
 
-        if(not(self.url)):
-            logger.warning('No url, wtf ?', self)
-            raise
+        if not self.url:
+            raise Exception('No url here, WTF ?')
 
         self.engine = create_engine(self.url, echo=echo)
-        Session = sessionmaker(bind=self.engine)
-        self.session = Session()
- 
- 
-    def instance(self, *args, **kwargs): 
- 
-        '''
-        Dummy method, cause several IDEs can not handel singeltons in Python
-        '''
- 
-        pass
+        session = sessionmaker(bind=self.engine)
+        self.session = session()
  
 class Model():
- 
-    '''
+    """
     This is a baseclass with delivers all basic database operations
-    '''
+    """
     def __init__(self, *args, **kwargs):
         if('fixed_characteristics' in kwargs):
             self.fixed_characteristics = kwargs['fixed_characteristics']
 
     def save(self):
+        """Save object to database, two steps : add and commit. 
+
+        To just commit, use update
+        """
         db = Db.instance()
         db.session.add(self)
         db.session.commit()
  
-    def saveMultiple(self, objects = []):
-        db = Db.instance()
-        db.session.add_all(objects)
-        db.session.commit()
+    #def saveMultiple(self, objects = None):
+    #    """Save multiple object at once
+    #    """
+    #    if objects is None:
+    #        objects = []
+
+    #    db = Db.instance()
+    #    db.session.add_all(objects)
+    #    db.session.commit()
  
     def update(self):
+        """Just perform a small update
+        """
         db = Db.instance()
         db.session.commit()
  
     def delete(self):
+        """Delete an object from a session
+        """
         db = Db.instance()
         db.session.delete(self)
         db.session.commit()
  
-    def queryObject(self):
+    def query(self):
+        """Perform a query
+        """
         db = Db.instance()
         return db.session.query(self.__class__)
 
-    # Create an object with characteristics, but dont commit it
     def new(self, *args, **kwargs):
+        """Create an object given characteristics, not committing it
+        """
         result = self.__class__()
         characteristics = kwargs.get('characteristics')
         # TODO : throw errors if no characteristics ?
@@ -117,21 +131,38 @@ class Model():
 
         return result
 
-    # Fetch a list of object having all the following characteristics
-    def newListFromCharacteristics(self, *args, **kwargs):
-        db = Db.instance()
-        query = self.queryObject()
+    def fetch_list(self, *args, **kwargs):
+        """Fetch a list of object having the same characteristics
+        """
+        query = self._handle_characteristics(*args, **kwargs)
+        return query.all()
+
+
+    def fetch(self, *args, **kwargs):
+        """Same as fetch_list but raise exception if multiple found
+        """
+        query = self._handle_characteristics(*args, **kwargs)
+        return query.one()
+
+    def _handle_characteristics(self, *args, **kwargs):
+        """Parse arguments for characteristics 
+        return a query that has been prepared
+        """
+        query = self.query()
         characteristics = kwargs.get('characteristics')
 
         try:
             getattr(self, 'fixed_characteristics')
-            if(not(characteristics)):
+            if not characteristics:
                 kwargs['characteristics'] = self.fixed_characteristics
             else:
                 for key, value in self.fixed_characteristics.items():
-                    if((key in characteristics) and (characteristics[key] != value)):
-                        logger.warning('You are trying to fetch a customer with wrong characteristics, going to fix %s to %s' % (key, value))
-
+                    if key in characteristics and characteristics[key] != value:
+                        message = 'Malformed characteristics. Fixing '
+                        message += ' {}'.format(key)
+                        message += ' from {}'.format(characteristics[key])
+                        message += ' to {}'.format(value)
+                        logger.warning(message)
                     characteristics[key] = value
 
         except AttributeError:
@@ -141,49 +172,41 @@ class Model():
         if(characteristics):
             for attr, value in characteristics.items():
                 # We have potentially other filter
-                if(isinstance(value, dict)):
+                if isinstance(value, dict):
                     operator = value['operator']
-                    fvalue   = value['value']
-                    logger.debug_debug("Going to append filter %s and operator %s=%s" % (attr, operator, fvalue))
-                    query = query.filter(getattr(self.__class__, attr).like(fvalue))
-
-
+                    fvalue = value['value']
+                    logger.debug_debug(
+                        "Going to append filter %s and operator %s=%s" % 
+                        (attr, operator, fvalue))
+                    # TODO : so far only like ...
+                    attr = getattr(self.__class__, attr)
+                    query = query.filter(attr.like(fvalue))
                 else:
-                    logger.debug_debug("Going to append filter %s=%s" % (attr, value))
+                    logger.debug_debug(
+                        "Going to append filter %s=%s" % (attr, value))
                     query = query.filter(getattr(self.__class__, attr)==value)
-        return query.all()
 
 
-
-
-    # Same as list but query.one() -> will raise an exception if multiple found
-    def newFromCharacteristics(self, *args, **kwargs):
-        db = Db.instance()
-        query = self.queryObject()
-        #logger.debug("newFromCharacteristicsOrCreate")
-        characteristics = kwargs.get('characteristics')
-        # TODO : throw errors if no characteristics
-        for attr, value in characteristics.items():
-            logger.debug_debug("Going to append filter %s=%s" % (attr, value))
-            query = query.filter(getattr(self.__class__, attr)==value)
-        return query.one()
-
-
-    # First newFromCharacteristics if no one found, the new and still dont commit
-    def newFromCharacteristicsOrCreate(self, *args, **kwargs):
+    def fetch_or_create(self, *args, **kwargs):
+        """Try to find an object in database.
+        If no object is found, create and commit one
+        """
         from sqlalchemy.orm.exc import NoResultFound
         try:
-            result = self.newFromCharacteristics(*args, **kwargs)
-        except NoResultFound as e:
+            result = self.fetch(*args, **kwargs)
+        except NoResultFound:
             result = self.new(*args, **kwargs)
+            result.save()
         except:
             raise
 
         return result
 
 
-    # TODO : make this better according to type ?
     def as_dict(self):
-           return {c.name: str(getattr(self, c.name)) for c in self.__table__.columns}
+        """To be able to dump data
+        """
+        columns = self.__table__.columns
+        return { c.name: str(getattr(self, c.name)) for c in columns }
 
 
