3933e7ac36108c8a10f1d12db7a64a3d5a79b186
Alkivi <admin@alkivi.fr>
2014-01-14 11:00:01 +0100

    pylint

--- common/__init__.py
+++ common/__init__.py
@@ -1,19 +1,22 @@
+"""Several helpers used to make alkivi life easier
+"""
 class Singleton(object):
- 
-    '''
-    Singelton class
-    '''
+    """Class to handle singleton
+    """
  
     def __init__(self, decorated):
         self._decorated = decorated
+        self._instance = None
  
     def instance(self, *args, **kwargs):
-        try:
-            return self._instance
- 
-        except AttributeError:
+        """Method to get the instance of a singleton
+
+        Create the instance if not used yet or restore it
+        """
+        if self._instance is None:
             self._instance = self._decorated(*args, **kwargs)
-            return self._instance
+
+        return self._instance
  
     def __call__(self, *args, **kwargs):
-        raise TypeError('Singletons must be accessed through the `Instance` method.')
+        raise TypeError('Singletons must be accessed through instance().')
