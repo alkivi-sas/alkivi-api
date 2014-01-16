96442e851c11fde695e50ead6ff06818194aa3bc
Alkivi <admin@alkivi.fr>
2014-01-16 12:14:10 +0100

    improving overall structure

--- l0/billing.py
+++ l0/billing.py
@@ -1,17 +1,70 @@
 from alkivi.common import sql
-from sqlalchemy import Column, Date, Integer, String, Index, ForeignKey, Float, DateTime
-from sqlalchemy.orm import relationship, backref
+from sqlalchemy import Column, Integer, String, Index, ForeignKey, Float, DateTime
+from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declared_attr, declarative_base
 from alkivi.common import logger
 
 Base = declarative_base()
+
+def has_changed(local_value, linxo_value, data_type=None):
+    """Perform comparaison according to type
+
+    Parameters
+    linxo_value comes from linxo_object : always str
+    local_value comes from database
+    """
+    if data_type is None:
+        return local_value != linxo_value
+    elif data_type is 'int':
+        return local_value != int(linxo_value)
+    elif data_type is 'float':
+        return local_value != float(linxo_value)
+    elif data_type is 'date':
+        from datetime import datetime
+        linxo_value = datetime.fromtimestamp(int(linxo_value))
+        return local_value != linxo_value
+    else:
+        raise Exception('wrong data_type')
+
+def format_linxo_data(linxo_value, data_type=None):
+    """see has_changed ...
+    """
+    if data_type is None:
+        return linxo_value
+    elif data_type == 'int':
+        return int(linxo_value)
+    elif data_type == 'float':
+        return float(linxo_value)
+    elif data_type == 'date':
+        from datetime import datetime
+        return datetime.fromtimestamp(int(linxo_value))
+    else:
+        raise Exception('wrong data_type')
+
+
+
+def check_changes(local, linxo, translation_dict, data_type=None):
+    """Generic function to call when updating object
+    """
+    changes = None
+
+    for local_key, linxo_key in translation_dict.iteritems():
+        if linxo_key in linxo:
+            value = getattr(local, local_key)
+            if has_changed(value, linxo[linxo_key], data_type):
+                value = format_linxo_data(linxo[linxo_key])
+                setattr(local, local_key, value)
+                if changes is None:
+                    logger.debug('key {} differs {} {}'.format(local_key, value, linxo[linxo_key]))
+                    changes = True
+    return changes
 
 class Db():
     instance = None
 
     def __init__(self, echo=False):
         # Will fetch credentials in a secureData file ... check /alkivi/common/sql.py
-        self = sql.Db.instance(useData='billing', echo=echo)
+        self = sql.Db.instance(use_data='billing', echo=echo)
         Base.metadata.create_all(self.engine)
 
 class BankAccount(sql.Model, Base):
@@ -23,19 +76,21 @@ class BankAccount(sql.Model, Base):
 
     # Table Scheme
     id = Column(Integer, primary_key = True)
-    accountGroupId = Column(Integer)
-    accountGroupName = Column(String(30))
-    accountNumber = Column(String(30))
+    account_group_id = Column(Integer)
+    account_group_name = Column(String(30))
+    account_namber = Column(String(30))
     name = Column(String(120))
     type = Column(String(30))
 
     # Indexes
     indexes = []
-    indexes.append(Index('idx_accountNumber', accountNumber))
+    indexes.append(Index('idx_account_namber', account_namber))
     indexes.append(Index('idx_type', type))
-    indexes.append(Index('idx_accountGroupId', accountGroupId))
-    indexes.append(Index('idx_accountGroupName', accountGroupName))
+    indexes.append(Index('idx_account_group_id', account_group_id))
+    indexes.append(Index('idx_account_group_name', account_group_name))
     
+    # Relationship
+    transaction = relationship("BankTransaction", backref="bank_accounts")
 
     # Database configuration
     @declared_attr
@@ -45,20 +100,66 @@ class BankAccount(sql.Model, Base):
 
     # Functions
     def __repr__(self):
-        return "<BankAccount ('%d','%s', '%s')>" % (self.id or 0, self.accountGroupName or 'empty', self.name or 'empty')
+        return "<BankAccount ('%d','%s', '%s')>" % (self.id or 0, self.account_group_name or 'empty', self.name or 'empty')
 
-    def new_from_linxo(self, object):
-        characteristics = { 'id' : object['id'] }
-        self = self.newFromCharacteristicsOrCreate(characteristics = characteristics)
+    def _get_translation_dict(self, data=None):
+        """Link between our object name and linxo object
+        """
+        if data is None:
+            return {
+                'account_group_name' : 'accountGroupName',
+                'account_namber': 'accountNumber',
+                'name': 'name',
+                'type': 'type' }
+        elif data is 'int':
+            return {
+                'account_group_id': 'accountGroupId',}
+        else:
+            raise Exception('wrong data {}'.format(data))
 
-        for key in ['accountGroupId', 'accountGroupName', 'accountNumber', 'name', 'type']:
-            setattr(self, key, object[key])
+    def _get_data_types(self):
+        """Return data_type use to translation linxo data to database
+        """
+        return [None, 'int']
+
+
+    def create_from_linxo(self, linxo):
+        """Create object take linxo data as parameter
+
+        Parameters
+        linxo : json dict from linxo api
+        """
+        characteristics = { 'id' : linxo['id'] }
+        self = self.fetch_or_create(characteristics=characteristics, no_save=True)
+
+        for data_type in self._get_data_types():
+            for local_key, linxo_key in self._get_translation_dict(data_type).iteritems():
+                value = format_linxo_data(linxo[linxo_key], data_type)
+                setattr(self, local_key, value)
 
         self.save()
         return self
 
+    def update_from_linxo(self, linxo):
+        """Check if they are any changes and commit only when changes
 
-class BankTransaction(sql.Model,Base):
+        Parameters
+        linxo : json dict from linxo api
+        """
+        changes = None
+        for data_type in self._get_data_types():
+            temp_changes = check_changes(self, linxo, self._get_translation_dict(data_type), data_type)
+            if changes is None and temp_changes:
+                changes = temp_changes
+
+        if changes:
+            logger.debug('updating object')
+            self.save()
+        else:
+            logger.debug('not updating object')
+        return self
+
+class BankTransaction(sql.Model, Base):
     __tablename__ = 'bank_transactions'
 
     # Need to instanciate db (singleton) 
@@ -67,26 +168,27 @@ class BankTransaction(sql.Model,Base):
 
     # Table Scheme
     id = Column(Integer, primary_key = True)
-    bankAccountId = Column(Integer)
-    openerpId = Column(Integer)
+    bank_account_id = Column(Integer, ForeignKey('bank_accounts.id'))
+    openerp_id = Column(Integer)
     amount = Column(Float, nullable=False)
-    budgetDate = Column(DateTime)
-    categoryId = Column(Integer)
+    budget_date = Column(DateTime)
+    category_id = Column(Integer)
     date = Column(DateTime)
     label = Column(String(32))
     notes = Column(String(255))
-    originalCategory = Column(Integer)
-    originalCity = Column(String(32))
-    originalDateAvailable = Column(DateTime)
-    originalDateInitiated = Column(DateTime)
-    originalLabel = Column(String(32))
-    originalThirdParty = Column(String(32))
+    original_category = Column(Integer)
+    original_city = Column(String(32))
+    original_date_available = Column(DateTime)
+    original_date_initiated = Column(DateTime)
+    original_label = Column(String(32))
+    original_third_party = Column(String(32))
 
     # Indexes
     indexes = []
-    indexes.append(Index('idx_bankAccountId', bankAccountId))
+    indexes.append(Index('idx_bank_account_id', bank_account_id))
+    indexes.append(Index('idx_openerp_id', openerp_id))
     indexes.append(Index('idx_date', date))
-    indexes.append(Index('idx_budgetDate', budgetDate))
+    indexes.append(Index('idx_budget_date', budget_date))
     indexes.append(Index('idx_amount', amount))
     
 
@@ -100,23 +202,70 @@ class BankTransaction(sql.Model,Base):
     # Functions
     def __repr__(self):
         return "<BankStatement ('%d', '%s', '%.2f')>" % (self.id or 0, self.label or '', self.amount or '0.0')
+    
+    def _get_translation_dict(self, data=None):
+        if data is None:
+            return {
+                'amount': 'amount',
+                'label': 'label',
+                'notes': 'notes',
+                'original_city': 'originalCity',
+                'original_label': 'originalLabel',
+                'original_third_party': 'originalThirdParty',}
+        elif data is 'int':
+            return {
+                'bank_account_id': 'bankAccountId',
+                'original_category': 'originalCategory',
+                'category_id': 'categoryId',}
+        if data is 'float':
+            return {
+                'amount': 'amount',}
+        elif data == 'date':
+            return {
+                'budget_date': 'budgetDate',
+                'date': 'date',
+                'original_date_available': 'originalDateAvailable',
+                'original_date_initiated': 'originalDateInitiated',
+            }
+        else:
+            raise Exception('wrong data {}'.format(data))
 
-    def new_from_linxo(self, object):
-        characteristics = { 'id' : object['id'] }
-        self = self.newFromCharacteristicsOrCreate(characteristics = characteristics)
+    def _get_data_types(self):
+        """Return data_type use to translation linxo data to database
+        """
+        return [None, 'int', 'float', 'date']
 
-        for key in ['bankAccountId', 'amount', 'categoryId', 'label', 'notes', 'originalCategory', 'originalCity', 'originalLabel', 'originalThirdParty']:
-            # According to linxo bank, we dont have the same info. Some keys might be missing
-            if key in object:
-                setattr(self, key, object[key])
+    def create_from_linxo(self, linxo):
+        """Create object take linxo data as parameter
 
-        # Special treatment for timestamp
-        from datetime import datetime
-        for key in ['budgetDate', 'date', 'originalDateAvailable', 'originalDateInitiated']:
-            # According to linxo bank, we dont have the same info. Some keys might be missing
-            if key in object:
-                value = datetime.fromtimestamp(int(object[key]))
-                setattr(self, key, value)
+        Parameters
+        linxo : json dict from linxo api
+        """
+        characteristics = { 'id' : linxo['id'] }
+        self = self.fetch_or_create(characteristics=characteristics, no_save=True)
 
+        for data_type in self._get_data_types():
+            for local_key, linxo_key in self._get_translation_dict(data_type).iteritems():
+                value = format_linxo_data(linxo[linxo_key], data_type)
+                setattr(self, local_key, value)
 
+        return self
+
+    def update_from_linxo(self, linxo):
+        """Check if they are any changes and commit only when changes
+
+        Parameters
+        linxo : json dict from linxo api
+        """
+        changes = None
+        for data_type in self._get_data_types():
+            temp_changes = check_changes(self, linxo, self._get_translation_dict(data_type), data_type)
+            if changes is None and temp_changes:
+                changes = temp_changes
+
+        if changes:
+            logger.debug('updating object')
+            self.save()
+        else:
+            logger.debug('not updating object')
         return self
